7a57fe33b3b348c0c71718cc424ef8be8a09a386
orpheusdark <117748842+orpheusdark@users.noreply.github.com>
2025-08-21 08:40:01 +0530

    Prepare for deployment

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// IMPORTANT: Replace with your actual Supabase URL and Anon Key
+// IMPORTANT: Actual Supabase URL and Anon Key
 const supabaseUrl = 'https://tetrczfzijwathyknwyk.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRldHJjemZ6aWp3YXRoeWtud3lrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzM1OTUxOSwiZXhwIjoyMDY4OTM1NTE5fQ.dWUmnJ1dLuZS5vOsNL4WDI_e_f4fzqVVfon9-3-wZjU';
 
